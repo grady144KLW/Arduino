6387955c817fe45fe87b7bdb0177b40ebbff44bc
grady144KLW <72415409+grady144KLW@users.noreply.github.com>
2020-10-18 06:53:28 -0700

    Add files via upload

---
+++ #3: Interacting With The Serial Monitor/sketch_oct15a.ino
@@ -0,0 +1,50 @@
+/*
+Declaring the pins of the LEDs
+ */
+ 
+int w1 = 5;
+int w2 = 8; 
+int wait_on_time = 500; // Declaring how long the LEDs should stop flashing
+int wait_off_time = 500;
+
+void setup() {
+Serial.begin(9600); // Starting the Serial Mointor at the baud rate of 9600
+/*  
+Declaring whether the LEDs are input or output
+ */
+
+pinMode(w1, OUTPUT);
+pinMode(w2, OUTPUT);
+}
+
+void loop() {
+  
+/*  
+declaring a counter integer
+then checking whether is greater or less than the number (5)
+incrementing the counting integer 
+*/
+
+for (int a = 1; a<=5; a++){
+Serial.print("First LED is blinking # "); // Prints the text inside of the speech marks
+Serial.println(a);  // Prints the text in a new line
+digitalWrite(w1, HIGH);
+delay(wait_on_time);
+digitalWrite(w1, LOW);
+delay(wait_off_time); 
+}
+
+/*
+This would blink the LEDs five times due to the counter integer
+ */
+
+for (int b = 1; b<=5; b++){
+Serial.print("Second LED is blinking # "); 
+Serial.println(b);  
+digitalWrite(w2, HIGH);
+delay(wait_on_time);
+digitalWrite(w2, LOW);
+delay(wait_off_time); 
+}
+
+}
